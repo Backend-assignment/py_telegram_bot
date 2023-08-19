02fb29d7b7135fedf084bf62388e0e00d85ec087
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-19 11:42:58 +0500

    added command handler

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from telegram import Bot, Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
 
 TOKEN ='5892121487:AAFJ8hXhSsCFBNMp-hHqFtAfwhO8RtCxdrM'
 
 def start(update: Update, context: CallbackContext):
+
+    chat_id = update.message.chat.id
+    bot = context.bot
+
+    bot.sendMessage(chat_id, "Welcome to echo bot")
+
+def main(update: Update, context: CallbackContext):
 
     chat_id = update.message.chat.id
     text = update.message.text
@@ -12,8 +19,10 @@ def start(update: Update, context: CallbackContext):
     bot.sendMessage(chat_id, text)
 
 updater = Updater(token=TOKEN)
-updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.text, main))
 
 updater.start_polling()
-
 updater.idle()
