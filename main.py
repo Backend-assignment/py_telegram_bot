653b104a11de4c3a047bf5ba9e5d07039267db86
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-19 11:39:06 +0500

    echo bot with py telegram bot

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from telegram import Bot
-TOKEN ='5913199109:AAG9-dVunDn9L499yI7bG-E8CFvBn2Rr6Ws'
-# Create a bot object using the token
-bot = Bot(token=TOKEN)  
-user = bot.get_me()
-print(user.first_name)
+from telegram import Bot, Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+
+TOKEN ='5892121487:AAFJ8hXhSsCFBNMp-hHqFtAfwhO8RtCxdrM'
+
+def start(update: Update, context: CallbackContext):
+
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+
+    bot.sendMessage(chat_id, text)
+
+updater = Updater(token=TOKEN)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, start))
+
+updater.start_polling()
+
+updater.idle()
